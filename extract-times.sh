78b689b0b8355ec93123b54614985f4e3ec6a1e3
Marc Brooker <mbrooker@amazon.com>
2019-01-04 17:52:33 +0000

    Emit CSV file of boot times along with the log file

--- extract-times.sh
+++ extract-times.sh
@@ -3,8 +3,10 @@
 #DATA_DIR=${1:-.}
 DATA_DIR="output"
 DEST="$PWD/data.log"
+CSV_DEST="$PWD/data.csv"
 
 rm -f $DEST
+echo "n,boot_ms" > $CSV_DEST
 
 pushd $DATA_DIR > /dev/null
 
@@ -14,6 +16,7 @@ for i in `seq 0 $COUNT`
 do
   boot_time=`grep Guest-boot fc-sb${i}-log | cut -f 2 -d '=' | cut -f 4 -d ' '`
   echo "$i boot $boot_time ms" >> $DEST
+  echo "$i,$boot_time" >> $CSV_DEST
 done
 
 popd > /dev/null
